10b27fd7413b123323801d3811b8f20131e96b3c
White_Chimney <white8chimney@gmail.com>
2022-11-13 14:57:46 +0800

    修改 python 脚本,增强可扩展性

---
+++ bib/python_scripts/bibTools.py
@@ -0,0 +1,166 @@
+class Person(object):
+    bib_name = ''
+    enable_website = False
+    def __init__(self,name):
+        self.name = name
+
+def personListFromConfigFile(config_fileName):
+    fConfig = open(config_fileName, 'r')
+    textList = fConfig.read().split('\n\n')
+
+    personList = []
+    for text in textList:
+        personInfoText = text.split('\n')
+        temp_person = Person(personInfoText[0].strip().replace(':',''))
+        for item in personInfoText:
+            if (item.find('bib_name') != -1):
+                temp_person.bib_name = item.strip().replace('bib_name: ','').replace('"','')
+                personList.append(temp_person)
+            if (item.find('webpage: "http://') != -1):
+                temp_person.enable_website = True
+    
+    return personList
+
+def importBib(bib_file):
+    fBib = open(bib_file, 'r')
+    bibList = []
+    tempBib = ''
+    line = fBib.readline()
+
+    while (line):
+        if (line[0] == "@"):
+            if (tempBib != ''):
+                bibList.append(tempBib)
+                tempBib = ''
+        tempBib += line
+        line = fBib.readline()
+    bibList.append(tempBib)
+
+    fBib.close()
+
+    return bibList
+
+def revert_author_name(author_name):
+    author_list = author_name.split(' and ')
+    reverted_name = ''
+    for name in author_list:
+        temp = name.split(', ')
+        reverted_name += temp[-1] + ' ' + temp[0] + ', '
+    reverted_name = reverted_name[0:-2]
+    return reverted_name
+
+def bib2html(bib_file, html_file, author_name):
+    bibList = importBib(bib_file)
+    findWork = False
+
+    fHtml = open(html_file,'w')
+    for bib in bibList:
+        if (bib.find(author_name) != -1):
+            if (not findWork):
+                fHtml.write('<h2 id="发表工作">发表工作</h2>\n<ol>\n')
+                findWork = True
+
+            fHtml.write('<li>\n<p>')
+
+            # author
+            author_start = bib.find('{',bib.find('author'))+1
+            author_end = bib.find('}',author_start)
+            author_text = bib[author_start:author_end]
+            author_text = revert_author_name(author_text)
+            author_text = author_text.replace(revert_author_name(author_name),'<strong><em>'+revert_author_name(author_name)+'</strong></em>')
+            author_text += ', '
+            fHtml.write(author_text)
+        
+            # title
+            title_start = bib.find('{',bib.find('title'))+1
+            title_end = bib.find('}',title_start)
+            title_text = bib[title_start:title_end]
+            title_text += ', '
+            fHtml.write(title_text)
+
+            # journal
+            journal_start = bib.find('{',bib.find('journal'))+1
+            journal_end = bib.find('}',journal_start)
+            journal_text = '<em>' + bib[journal_start:journal_end] + '</em>'
+            journal_text += ', '
+            fHtml.write(journal_text)
+
+            # volume
+            index = bib.find('volume')
+            if (index != -1):
+                volume_start = bib.find('{',index)+1
+                volume_end = bib.find('}',volume_start)
+                volume_text = bib[volume_start:volume_end]
+            else:
+                volume_text = ''
+            fHtml.write(volume_text)
+
+            # number
+            index = bib.find('number')
+            if (index != -1):
+                number_start = bib.find('{',index)+1
+                number_end = bib.find('}',number_start)
+                number_text = '(' + bib[number_start:number_end] + ')'
+                number_text += ', '
+            else:
+                number_text = ''
+            fHtml.write(number_text)
+
+            # pages
+            index = bib.find('pages')
+            if (index != -1):
+                pages_start = bib.find('{',index)+1
+                pages_end = bib.find('}',pages_start)
+                pages_text = bib[pages_start:pages_end]
+                pages_text += ', '
+            else:
+                pages_text = ''
+            fHtml.write(pages_text)
+
+            # ISSN
+            index = bib.find('ISSN')
+            if (index != -1):
+                ISSN_start = bib.find('{',index)+1
+                ISSN_end = bib.find('}',ISSN_start)
+                ISSN_text = bib[ISSN_start:ISSN_end]
+                ISSN_text += ','
+            else:
+                ISSN_text = ''
+            fHtml.write(ISSN_text)
+
+            # year
+            year_start = bib.find('{',bib.find('year'))+1
+            year_end = bib.find('}',year_start)
+            year_text = bib[year_start:year_end]
+            year_text += '.'
+            fHtml.write(year_text)
+
+    fHtml.write('</p>\n</li>\n</ol>')
+    fHtml.close()
+
+
+def sortBibForWebpage(orig_bib_fileName, sorted_bib_fileName):
+    origBib = importBib(orig_bib_fileName)
+    fOrderedBib = open(sorted_bib_fileName, 'w')
+    order = 10000
+    for bib in origBib:
+        index = bib.find('featured')
+        if (index != -1):
+            if (bib[bib.find('{',index)+1:bib.find('}',index)] == 'true'):
+                order -= 1
+                yearToWrite = str(order)
+        else:
+            yearToWrite = bib[bib.find('{',bib.find('year'))+1:bib.find('}',bib.find('year'))]
+        ordered_bib = bib.replace('year','year={'+yearToWrite+'},\n  realyear')
+        fOrderedBib.write(ordered_bib)
+    fOrderedBib.close()
+
+def extractPersonalBib(bib_name, original_bib_file, personal_bib_file):
+    origBib = importBib(original_bib_file)
+
+    fPersonalBib = open(personal_bib_file,'w')
+    for bib in origBib:
+        if (bib.find(bib_name) != -1):
+            fPersonalBib.write(bib)
+    
+    fPersonalBib.close()
---
+++ bib/python_scripts/extractPersonalBib.py
@@ -0,0 +1,31 @@
+# 基于 QKDLAB.bib 更新个人 bib 数据以搭建网站
+
+import bibTools
+from bibTools import Person
+
+site_dir = '/home/data/BA21038029/ssh_update_bib_test/research-group-web/'
+orig_bib_fileName = site_dir + 'bib/QKDLAB.bib'
+sorted_bib_fileName = site_dir + 'bib/pubs.bib'
+config_fileName = site_dir + '_data/people.yml'
+personalbib_path = site_dir + 'bib/personal_bib/'
+personalhtml_path = site_dir + '_includes/personal_bib_autogen/'
+
+# 对原 bib 文件进行排序
+# 标记有 featured = {true} 的项进行加权排序
+# 其余项使用年份以默认排序
+
+# 从配置文件 people.yml 生成 person list
+personList = bibTools.personListFromConfigFile(config_fileName)
+
+# 将原始 bib 文件排序，用以在网页以定序显示
+bibTools.sortBibForWebpage(orig_bib_fileName, sorted_bib_fileName)
+
+# 处理 bib 条目
+for person in personList:
+    author_name = person.bib_name
+    personalbib_name = personalbib_path + person.name + '.bib'
+    bibTools.extractPersonalBib(author_name,orig_bib_fileName,personalbib_name)
+    if (person.enable_website):
+        personalhtml_name = personalhtml_path + person.name + '.bib.html'
+        bibTools.bib2html(personalbib_name,personalhtml_name,author_name)
+        
